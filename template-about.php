903fcd3f0f424e4f56c0bc24b1559b94c0c5a582
Remy <davisonren@gmail.com>
2020-09-24 14:19:27 -0400

    drafted about page

---
+++ template-about.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: About Us
+*/
+?>
+
+<?php get_header()?>
+
+<main class="container-fluid" id="about-page">
+    <h1><?php the_title();?></h1>
+
+    <section class="about-section">
+      <?php get_template_part('includes/section', 'content');?>
+    </section>
+</main>
+
+<?php get_footer()?>
