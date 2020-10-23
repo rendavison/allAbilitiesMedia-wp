98cbd772eb70fc5d1ee1da35627b21db9d38f6c2
Remy <davisonren@gmail.com>
2020-10-23 09:34:09 -0400

    added team page

---
+++ template-team.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Our Team
+*/
+?>
+
+<head>
+  <title><?php the_title();?> - All-Abilities Media</title>
+</head>
+
+<?php get_header()?>
+
+<main class="container-fluid" id="text-page" style="padding: 0; margin: 0">
+  <?php get_template_part('includes/section', 'header');?>
+
+  <section class="team-section">
+    <?php get_template_part('includes/section', 'content');?>
+  </section>
+</main>
+
+<?php get_footer()?>
